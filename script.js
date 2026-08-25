f544b3f1071131cd14a67b1418b388b40ae8df4c
Yunana <lucia1007@naver.com>
2026-08-26 02:31:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -39,9 +39,9 @@ const maxSize = Math.max(window.innerWidth, window.innerHeight);
 
 
 function showPaper() {
-  document.getElementByClassname("clipGlow").style.display = "none";
-  document.getElementByClassname("clipImage").style.display = "none";
-  document.getElementByClassname("lettersImage").style.display = "none";
+  document.getElementsByClassname("clipGlow").style.display = "none";
+  document.getElementsByClassname("clipImage").style.display = "none";
+  document.getElementsByClassname("lettersImage").style.display = "none";
   document.querySelector(".letterPaper").style.display = "flex";
 }
 
